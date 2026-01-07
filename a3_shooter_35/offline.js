c01bff55a589438e37a3589d0bd10eefa74394ae
s41139 <s41139@bj.ac.th>
2026-01-07 11:15:34 +0700

    Add files via upload

--- a3_shooter_35/offline.js
+++ a3_shooter_35/offline.js
@@ -1,15 +1,15 @@
 ﻿{
-	"version": 1765506415,
+	"version": 1767759147,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-3.4.1.min.js",
 		"offlineClient.js",
-		"images/bg.png",
+		"images/background.png",
 		"images/player-sheet0.png",
 		"images/monster-sheet0.png",
 		"images/bullet-sheet0.png",
-		"images/explode-sheet0.png",
+		"images/explosion-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
