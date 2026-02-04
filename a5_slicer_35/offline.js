e9989640b7e107e31da0d9070deb6bd107c01380
s41139 <s41139@bj.ac.th>
2026-02-04 11:40:35 +0700

    Add files via upload

---
+++ a5_slicer_35/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1770180008,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/botcut-sheet0.png",
+		"images/player-sheet0.png",
+		"images/topcut-sheet0.png",
+		"images/strawberry-sheet0.png",
+		"images/grape_w-sheet0.png",
+		"images/grape_top-sheet0.png",
+		"images/grape_bot-sheet0.png",
+		"images/spawn-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
